ed9f861b18e2f4f2480ea671cfb8add47aa72290
B3ND3L <delahais.b@gmail.com>
2023-04-06 23:31:46 +0200

    POC : Refacto + Add event on_presence_update

--- nick_bot/main.py
+++ nick_bot/main.py
@@ -17,44 +17,65 @@ conf_file = open("/config/config.yaml")
 yaml = YAML()
 config = yaml.load(conf_file)
 
+# REGEX : @nick_bot rename @someone en new_name
+RENAME_REGEX = re.compile('<@\d+> rename <@\d+> en (.*)')
+# REGEX : @nick_bot ajoute mon battletag
+ADD_BATTLETAG_REGEX = re.compile('<@\d+> add battletag (.*)')
+
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
 
 @client.event
-async def on_message(message):
+async def on_message(message: Message):
     if message.author == client.user:
         return
+
+        match_rename = re.match(RENAME_REGEX, message.content)
+        new_name = match_rename.group(1)
+
+        match_add_battletag = re.search(ADD_BATTLETAG_REGEX, message.content)
+        battletag = match_add_battletag.group(1)
 
     if message.mentions:
         mentioned_ids = list(map(lambda mention: mention.id, message.mentions))
         if client.user.id in mentioned_ids:
             mentioned_ids.remove(client.user.id)
-            return_message = await do_action(message, mentioned_ids)
+            return_message = ''
+            if new_name:
+                return_message = await renaming(new_name, mentioned_ids)
+            elif battletag:
+                return_message = await add_battletag(battletag, message.author, mentioned_ids)
             await message.channel.send(return_message)
 
 
-async def do_action(message: Message, mentioned_ids: list) -> str:
+@client.event
+async def on_presence_update(member, before, after):
+    print(member)
+    for activity in after.activities:
+        print(activity.name)
 
-    match = re.search('<@\d+> rename <@\d+> en (.*)', message.content)
 
-    new_name = match.group(1)
+async def add_battletag(battletag: str, user, mentioned_ids):
+    print(battletag)
 
-    if new_name:
-        members = client.get_all_members()
-        for member in members:
-            for id in mentioned_ids:
-                if member.id == id:
-                    old_name = member.name
-                    print(f'FOUND IT ! {old_name}')
-                    try:
-                        await member.edit(nick=new_name)
-                    except Exception:
-                        return f'Je n\'ai pas les droits de rename {old_name}'
+
+async def renaming(new_name: str, mentioned_ids: list) -> str:
+    members = client.get_all_members()
+    for member in members:
+        for id in mentioned_ids:
+            if member.id == id:
+                old_name = member.name
+                print(f'FOUND IT ! {old_name}')
+                try:
+                    await member.edit(nick=new_name)
+                except Exception:
+                    return f'Je n\'ai pas les droits de rename {old_name}'
 
         return f'Renommage de {old_name} en {new_name}'
-    else :
+    else:
         return 'Bad command :('
 
 
