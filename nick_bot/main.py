67d0413e5067b882c24759f149e9b55ba53d399b
Benjamin DELAHAIS <bdelahais@solocal.com>
2024-03-25 15:50:31 +0100

    Remove Overwatch session tracker

--- nick_bot/main.py
+++ nick_bot/main.py
@@ -5,10 +5,8 @@ from discord import Member
 from discord import Message
 from ruamel.yaml import YAML
 
-from nick_bot.services.BattletagService import BattletagService
 from nick_bot.services.MessageService import MessageService
 from nick_bot.services.RenameService import RenameService
-from nick_bot.services.SessionService import SessionService
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -20,10 +18,8 @@ client = discord.Client(intents=intents)
 yaml = YAML()
 config = yaml.load(open("../config/config.yaml"))
 
-battletag_service = BattletagService(config)
 rename_service = RenameService(client)
-message_service = MessageService(battletag_service, rename_service)
-session_service = SessionService(config, battletag_service)
+message_service = MessageService(rename_service)
 
 logger = logging.getLogger('discord')
 
@@ -48,17 +44,5 @@ async def on_message(message: Message):
     response = await message_service.read_message(client.user.id, message)
     if response:
         await message.channel.send(response)
-
-
-@client.event
-async def on_presence_update(before: Member, after: Member):
-    """
-    When a user status is updated
-    :param before:
-    :param after:
-    :return:
-    """
-    await session_service.on_presence(before, after)
-
 
 client.run(config['token'])
--- nick_bot/services/MessageService.py
+++ nick_bot/services/MessageService.py
@@ -1,8 +1,6 @@
 import re
 
 from discord import Message
-
-from nick_bot.services.BattletagService import BattletagService
 from nick_bot.services.RenameService import RenameService
 
 
@@ -14,20 +12,14 @@ class MessageService:
 
     # REGEX : @nick_bot renomme @someone en new_name
     __RENAME_REGEX = re.compile('<@\d+>\s+(renomme|rename)\s+<@\d+>\s+(en|to)\s+(.*)')
-    # REGEX : @nick_bot ajoute mon <battletag>
-    __ADD_BATTLETAG_REGEX = re.compile('<@\d+>\s+(add|ajoute)\s+(my|mon)\s+battletag\s+(.*)')
-    # REGEX : @nick_bot retire mon <battletag>
-    __REMOVE_BATTLETAG_REGEX = re.compile('<@\d+>\s+(remove|retire)\s+(my|mon)\s+battletag\s+(.*)')
     # REGEX : @nick_bot help
     __HELP = re.compile('<@\d+>\s+(help|aide)')
 
-    def __init__(self, battletag_service: BattletagService, rename_service: RenameService):
+    def __init__(self,rename_service: RenameService):
         """
         Constructor
-        :param battletag_service:
         :param rename_service:
         """
-        self._battletag_service = battletag_service
         self._rename_service = rename_service
 
     async def read_message(self, client_id: int, message: Message) -> str:
@@ -40,8 +32,6 @@ class MessageService:
 
         content = message.content
         match_rename = re.match(self.__RENAME_REGEX, content)
-        match_add_battletag = re.match(self.__ADD_BATTLETAG_REGEX, content)
-        match_remove_battletag = re.match(self.__REMOVE_BATTLETAG_REGEX, content)
         match_help = re.match(self.__HELP, content)
 
         return_message = 'Je ne sais pas quoi répondre.'
@@ -53,12 +43,6 @@ class MessageService:
                 if match_rename:
                     new_name = match_rename.group(3)
                     return_message = await self._rename_service.renaming(new_name, mentioned_ids)
-                elif match_add_battletag:
-                    battletag = match_add_battletag.group(3)
-                    return_message = self._battletag_service.add_battletag(battletag, message.author.name)
-                elif match_remove_battletag:
-                    battletag = match_remove_battletag.group(3)
-                    return_message = self._battletag_service.remove_battletag(battletag, message.author.name)
                 elif match_help:
                     return_message = '''
 Voici les commandes que tu peux tapper:
@@ -66,9 +50,5 @@ Pour recevoir de l'aide :
 `help`
 Pour renommer quelqu'un:
 `renomme @name en nouveau_nom`
-Pour ajouter un battletag:
-`ajoute mon battletag MONBATTLETAG#1234`
-Pour retirer un battletag:
-`retire mon battletag MONBATTLETAG#1234`
                     '''
                 return return_message
